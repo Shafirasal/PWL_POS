71c3cc76ea4b423b23e3b801f769d972f7d2647b
Shafirasal <shafirasalsabila15@gmail.com>
2024-09-23 09:58:46 +0700

    praktikum 2.1 dan  2.2 Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,16 +27,35 @@ class UserController extends Controller
         //     'password' => Hash::make('12345')
         // ];
 
-         $data = [
-            'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+
+        // $user = UserModel:: find(1); // mencari dengan primary key 1
+        // $user = UserModel:: where('level_id', 1) -> first();
+        // $user = UserModel:: firstWhere('level_id', 1) -> first();
+
+            
+        // $user = UserModel:: findOr(1,['username', 'nama'], function(){
+        //     abort(404);
+        // });
+
+        // $user = UserModel:: findOr(20,['username', 'nama'], function(){
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOrFail(1);   
+        
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+
+
+        // // coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
