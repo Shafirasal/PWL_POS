6dbc16ea6e2e393b4a0c97842541ed9aa89c7a33
Shafirasal <shafirasalsabila15@gmail.com>
2024-10-31 22:03:09 +0700

    JS 10-PRAKTIKUM 3 - RESTful API Logout

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', App\http\Controllers\Api\RegisterController:: class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login'); //
 Route::get('/user', App\Http\Controllers\Api\LoginController::class)->name('login'); //
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');  //logout
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
